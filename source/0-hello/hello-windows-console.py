0c95db9b160e0fa16361134f702cf3b83b9d7a05
le74ndro <le74ndro@gmail.com>
2018-09-12 23:51:50 -0300

    Source: hello-windows-console

---
+++ source/0-hello/hello-windows-console.py
@@ -0,0 +1,10 @@
+# If double-click and start from Windows folder
+print("Hello, Windows Console!")
+# not have the console window disappear
+# add following code to the bottom of each script
+
+# stops console from exiting
+end_prog = ""
+while end_prog != "q":
+    end_prog = input("type q to quit")
+
