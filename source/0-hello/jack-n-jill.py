47d0eb622859b69bc4937fdb75309ffb37e5ad47
le74ndro <le74ndro@gmail.com>
2018-09-14 23:38:12 -0300

    Note: jack-n-jill

--- source/0-hello/jack-n-jill.py
+++ source/0-hello/jack-n-jill.py
@@ -3,4 +3,5 @@ print("to fetch a pail of water;")
 print("Jack fell down, and broke his crown,")
 print("and Jill came tumbling after.")
 # The computer keeps looking at each line, follows the command and then go to the next line.
+# The computer keeps running commands until it reacher the end of the program.
 
